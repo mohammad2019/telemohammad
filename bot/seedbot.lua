33a9bdd412556f785ecf0efde43bde9e96bb78a5
mohammad2019 <mohammadhaselmehri2016@gmail.com>
2016-01-08 22:33:46 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {150575718,0,tonumber(our_id},--Sudo users
+    sudo_users = {150575718,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {data = 'data/moderation.json'},--Realms Id
     moderation = {data = 'data/moderation.json'},
